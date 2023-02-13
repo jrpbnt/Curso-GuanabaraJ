e8c8ebe880b0af4ceae58e9f5fda1fd79b031e0d
jrpbnt <78651955+jrpbnt@users.noreply.github.com>
2023-02-13 17:03:17 -0300

    operadores aritimedicos Java8
    formas de tirar media ,Operadores em java ,e de sorteio ou pegar numeros aleatorios java e de arredondamento

---
+++ Operadoresaritmedicos/src/main/java/Operadoresaritmedicos.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author jrpbnt
+ */
+public class Operadoresaritmedicos {
+
+    public static void main(String[] args) {
+        System.out.println("ACERTO MIZERAVI");
+       
+        // Metodos de arredondamento ceil florr round 
+       /*   float v = 8.9f;
+       int ar =  (int) Math.floor(v );
+        System.out.println(ar)*/
+        
+        
+        
+         
+      //Media de dois numeros   
+     /* int n1 = 3;
+      int n2 = 5;
+      float m = (n1 + n2)/2;
+        System.out.println("A media é igual a " + m);*/
+     
+       // Operadores Unarios
+       /* int numero = 10;
+       int valor = 4 + numero--; 
+        System.out.println(valor);
+        System.out.println(numero);*/
+       
+       // Operadores de atribuição
+      /* int x  = 4;
+       x += 2; // x = x + 2 
+        System.out.println(x);*/
+      
+      //pegar un numero aleatorio entre 10 e 5
+      /*double ale = Math.random();
+      int n = (int) (5 + ale * (10-5));
+        System.out.println(n);*/
+        
+    
+       
+       
+       
+        
+    }
+}
