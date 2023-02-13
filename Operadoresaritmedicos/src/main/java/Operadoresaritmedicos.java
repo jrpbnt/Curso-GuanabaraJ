0d16279a67ee184bd14e5ff270974cbc9c7a3613
jrpbnt <78651955+jrpbnt@users.noreply.github.com>
2023-02-13 09:56:40 -0300

    UM exemplo de operadores aritmedicos EM java
    exemplo 1

---
+++ Operadoresaritmedicos/src/main/java/Operadoresaritmedicos.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author jrpbnt
+ */
+public class Operadoresaritmedicos {
+
+    public static void main(String[] args) {
+        System.out.println("ACERTO MIZERAVI");
+      int n1 = 3;
+      int n2 = 5;
+      float m = (n1 + n2)/2;
+        System.out.println("A media é igual a " + m);
+        
+        
+        
+    }
+}
