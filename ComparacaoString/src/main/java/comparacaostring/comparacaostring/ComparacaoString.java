dfbc5595ba5e3d91397bc37879c49b47cf3571a6
jrpbnt <78651955+jrpbnt@users.noreply.github.com>
2023-02-15 17:55:13 -0300

    comparador de strings

---
+++ ComparacaoString/src/main/java/comparacaostring/comparacaostring/ComparacaoString.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package comparacaostring.comparacaostring;
+
+/**
+ *
+ * @author jrpbnt
+ */
+public class ComparacaoString {
+
+    public static void main(String[] args) {
+        System.out.println("Lucramo");
+        String nome1 = "Gustavo";
+        String nome2 = "Gustavo";
+        String nome3 = new String("Gustavo");
+        String res ;
+        res = (nome1.equals(nome3))?"igual":"Diferente";
+        System.out.println(res);
+         
+        
+        
+        
+    }
+    
+}
