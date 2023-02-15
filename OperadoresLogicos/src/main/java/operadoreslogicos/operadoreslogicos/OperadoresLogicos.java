5a1aa8bad2bc3bacc824a27e46177da73b69df70
jrpbnt <78651955+jrpbnt@users.noreply.github.com>
2023-02-15 17:56:40 -0300

    Exemplo de operadores logicos
    Operadores logicos

---
+++ OperadoresLogicos/src/main/java/operadoreslogicos/operadoreslogicos/OperadoresLogicos.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package operadoreslogicos.operadoreslogicos;
+
+/**
+ *
+ * @author jrpbnt
+ */
+public class OperadoresLogicos {
+
+    public static void main(String[] args) {
+        System.out.println("Nigeryundayoooo");
+        
+        int x,y,z;
+        x = 4;
+        y = 7;
+        z = 12;
+        boolean r;
+        r = (x<y ^ y>z)?true:false;
+        
+        
+        System.out.println(r);
+         
+        
+    }
+}
