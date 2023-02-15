55b6345ef2054b5e372e5f9ad070d885b290806a
jrpbnt <78651955+jrpbnt@users.noreply.github.com>
2023-02-15 17:57:06 -0300

    Operadores Ternarios

---
+++ OperadorTernario/src/main/java/operadorternario/operadorternario/OperadorTernario.java
@@ -0,0 +1,23 @@
+   /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package operadorternario.operadorternario;
+
+/**
+ *
+ * @author jrpbnt
+ */
+public class OperadorTernario {
+
+    public static void main(String[] args) {
+        System.out.println("Vencemo");
+        int n1,n2,r;
+        n1 = 14;
+        n2 = 8;
+        r = (n1>n2)?n1+n2:n1-n2;
+            
+        System.out.println(r);
+        
+    }
+}
