c19509e65357e607c677720c67baeb2f703d9ae9
jrpbnt <78651955+jrpbnt@users.noreply.github.com>
2023-02-16 18:02:21 -0300

    um progama que fala sua media de 2 notas
    e se está aprovado ou n

---
+++ progamamedia/src/main/java/media/progamamedia/Progamamedia.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package media.progamamedia;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jrpbnt
+ */
+public class Progamamedia {
+
+    public static void main(String[] args) {
+        System.out.println("NICEEEEEEE");
+                Scanner teclado = new Scanner(System.in);
+                System.out.println("Primeira nota");
+                float n1 = teclado.nextFloat();
+                System.out.println("Segunda  nota");
+                float n2 = teclado.nextFloat();
+                float m = (n1+n2)/2;
+                System.out.println("Sua media foi" + m);
+                if(m>9) {
+                    System.out.println("Parabens, pequeno,ganhafoto!");
+                    
+
+                    
+                }
+                }
+    
+                
+        
+        
+
+        
+        
+       
+        
+        
+    }
+
