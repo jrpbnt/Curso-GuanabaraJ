596248f5ed18896d5f3f410ae8a509f5818119d7
jrpbnt <78651955+jrpbnt@users.noreply.github.com>
2023-02-22 17:40:48 -0300

    Exercicios de Repetição com teste no final

---
+++ Repeticao01/src/main/java/com/cursoemvideo/repeticao01/Repeticao01.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.cursoemvideo.repeticao01;
+
+/**
+ *
+ * @author jrpbnt
+ */
+public class Repeticao01 {
+
+    public static void main(String[] args) {
+        System.out.println("Vencemo");
+        int cc = 0;
+        do {
+            System.out.println(cc);
+            cc++; 
+        }while (cc<4);
+        
+        
+         
+                
+        
+        
+        
+    }
+}
