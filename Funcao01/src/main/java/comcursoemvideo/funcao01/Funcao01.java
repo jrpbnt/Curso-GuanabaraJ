fa0932597ad675674a616ef15ffc47cd0fa54c59
jrpbnt <78651955+jrpbnt@users.noreply.github.com>
2023-02-22 17:37:40 -0300

    Exercicio Fatorial

---
+++ Funcao01/src/main/java/comcursoemvideo/funcao01/Funcao01.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package comcursoemvideo.funcao01;
+
+/**
+ *
+ * @author jrpbnt
+ */
+public class Funcao01 {
+
+    public static void main(String[] args) {
+        System.out.println("bites the dust!!!!");
+        Fatorial f = new Fatorial();
+        f.setValor(5);
+        System.out.print(f.getFormula());
+        System.out.print(f.getFatorial());
+        
+            
+        
+    }
+}
