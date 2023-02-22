ccd909eb7983a1a6d8d6d0abce7bc49225bd22a7
jrpbnt <78651955+jrpbnt@users.noreply.github.com>
2023-02-22 17:41:56 -0300

    Exercicio sobre metodos

---
+++ TesteFuncao02/src/main/java/comcursoemvideo/testefuncao02/Operaçoes.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package comcursoemvideo.testefuncao02;
+
+/**
+ *
+ * @author jrpbnt
+ */
+public class Operaçoes {
+    
+      public static String contador(int i,int f){
+         
+       String s = "";
+       for(int c = i;c <= f ; c++) {
+           
+           
+       }
+       return s;
+       
+     }   
+    
+    
+         
+    
+    
+           }
+
+
---
+++ TesteFuncao02/src/main/java/comcursoemvideo/testefuncao02/TesteFuncao02.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package comcursoemvideo.testefuncao02;
+
+/**
+ *
+ * @author jrpbnt
+ */
+public class TesteFuncao02 {
+
+    public static void main(String[] args) {
+        System.out.println("Vai começar a contagem");
+        System.out.println(Operaçoes.contador(1,5));
+    }
+}
