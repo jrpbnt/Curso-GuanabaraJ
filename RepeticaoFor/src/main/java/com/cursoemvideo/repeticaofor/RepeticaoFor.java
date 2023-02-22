e111fbdede4e2720716254bf0485927cb1968543
jrpbnt <78651955+jrpbnt@users.noreply.github.com>
2023-02-22 17:41:28 -0300

    exercicio sobre for int

---
+++ RepeticaoFor/src/main/java/com/cursoemvideo/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.cursoemvideo.repeticaofor;
+
+/**
+ *
+ * @author jrpbnt
+ */
+public class RepeticaoFor {
+
+    public static void main(String[] args) {
+        System.out.println("Ganhemo");
+        for (int cc=0; cc<101; cc+=10) {
+            System.out.println(cc);
+            
+        }
+    }
+}
