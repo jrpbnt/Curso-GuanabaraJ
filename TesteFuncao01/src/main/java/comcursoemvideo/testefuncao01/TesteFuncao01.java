353ced74f4727987940e088ab1d05295498e6a27
jrpbnt <78651955+jrpbnt@users.noreply.github.com>
2023-02-22 17:41:43 -0300

    Exercicio sobre metodos

---
+++ TesteFuncao01/src/main/java/comcursoemvideo/testefuncao01/TesteFuncao01.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package comcursoemvideo.testefuncao01;
+
+/**
+ *
+ * @author jrpbnt
+ */
+public class TesteFuncao01 {
+    
+        
+   static int soma(int a ,int b){
+       int s = a + b;
+       return s;
+       
+   }
+     
+    
+
+    public static void main(String[] args) {
+        System.out.println("Começou o progama");
+        int sm = soma(5,2);
+        System.out.println("A soma vale " + sm);
+        
+    
+       }
+    
+    }
